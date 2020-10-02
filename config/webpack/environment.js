f82bbaa421fc37a7269741630d88f6ab4ec33621
jenyau <jekharitonov@gmail.com>
2020-10-02 23:03:33 +0300

    carousel not fixed and deploy

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,9 +1,5 @@
 const { environment } = require('@rails/webpacker')
 
-environment.loaders.get('sass').use.splice(-1, 0, {
-  loader: 'resolve-url-loader'
-});
-
 const webpack = require('webpack')
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
